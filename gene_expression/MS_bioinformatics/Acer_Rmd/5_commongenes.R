f998cdf08c5aa650f0967de17c02ff9d1900914b
Allyson DeMerlis <allyson.demerlis@gmail.com>
2023-09-29 10:30:19 -0400

    Update 5_commongenes.R

--- gene_expression/MS_bioinformatics/Acer_Rmd/5_commongenes.R
+++ gene_expression/MS_bioinformatics/Acer_Rmd/5_commongenes.R
@@ -8,24 +8,31 @@ library(RColorBrewer)
 
 
 #### DESEQ IMPORT ####
-load("ZRData_files/pvals.RData")
+load("RData_files/pvals.RData")
 
 load("RData_files/control0_control29_lpv.RData")
-control0_control29.p <- control0_control29.p %>% rename("lpv_c0c29" = lpv)
+control0_control29.p <- control0_control29.p %>% dplyr::rename("lpv_c0c29" = lpv) #40,443 genes
 
 load("RData_files/variable0_control0_lpv.RData")
-variable0_control0.p <- variable0_control0.p %>% rename("lpv_v0c0" = lpv)
+variable0_control0.p <- variable0_control0.p %>% dplyr::rename("lpv_v0c0" = lpv) #20,952 genes
 
 load("RData_files/variable0_variable29_lpv.RData")
-variable0_variable29.p <- variable0_variable29.p %>% rename("lpv_v0v29" = lpv)
+variable0_variable29.p <- variable0_variable29.p %>% dplyr::rename("lpv_v0v29" = lpv) #36,731 genes
 
 load("RData_files/variable29_control29_lpv.RData")
-variable29_control29.p <- variable29_control29.p %>% rename("lpv_v29c29" = lpv)
+variable29_control29.p <- variable29_control29.p %>% dplyr::rename("lpv_v29c29" = lpv) #30,233 genes
 
 #### DEG MATCHING ####
 
-# These sections of code do several things: 1) join common DEGs across experiments with -log10(pval), 
-#2) filter by 0.1 pval cutoff (log10(0.1)=1), 3)  adds gene annotations, and 4) then pulls on corresponding KOG classes
+# These sections of code do several things: 1) join common DEGs across experiments with -log10(padj), 
+#2) filter by 0.1 padj cutoff (log10(0.1)=1), 3)  adds gene annotations, and 4) then pulls on corresponding KOG classes
+
+# NOTE: normally a -log10 transformation of a p-value between 0 and 1 would not result in a negative number. However, when the above files 
+#were generated, (i.e. control0_control29.p), the stat of the directionality of the expression pattern (i.e. negative or positive) was encoded
+#in the lpv value manually. The exact lines of code for the transformations were:
+#variable29_control29.p$lpv=-log(source[,"padj"],10)
+#variable29_control29.p$lpv[source$stat<0]=variable29_control29.p$lpv[source$stat<0]*-1
+#This is why the absolute value of lpv is taken for obtaining genes of significance >= 1 (which corresponds to alpha = 0.1)
 
 # first see if there are any shared genes with control0vs29 and variable0vs29
 control0_control29.p %>%
@@ -42,39 +49,21 @@ control0_control29.p %>%
                        quote="", fill=FALSE) %>%
               mutate(gene = V1,
                      KOG = V2) %>%
-              dplyr::select(-V1, -V2), by = "gene") -> c0c29_v0v29 #there are ~11,000 shared genes (cut-off is p-value of 0.1 -- NOT p-adjusted but raw p-value, idk why)
-
-# list of genes (FDR p-adj < 0.1) shared by C0/C29 and V0/V29
-
-deg_control %>%
-  inner_join(variable0_variable29.p, by = "gene") %>%
-  filter(abs(lpv_c0c29) >= 1 & abs(lpv_v0v29) >= 1) %>%
-  left_join(read.table(file = "~/OneDrive - University of Miami/NOAA ERL/stress hardening 2022/gene expression/Acervicornis_annotatedTranscriptome/Acervicornis_iso2geneName.tab",
-                       sep = "\t",
-                       quote="", fill=FALSE) %>%
-              mutate(gene = V1,
-                     annot = V2) %>%
-              dplyr::select(-V1, -V2), by = "gene")
-
-as.data.frame(control0_control29) %>% 
-  mutate(contrast = "control0_control29") %>% 
-  rownames_to_column("gene") %>% 
-  filter(padj < 0.1) -> df_C0C29
-
-as.data.frame(variable0_variable29) %>% 
-  mutate(contrast = "variable0_variable29") %>% 
-  rownames_to_column("gene") -> df_V0V29
-
-df_C0C29 %>% inner_join(df_V0V29, by = "gene")
-  
+              dplyr::select(-V1, -V2), by = "gene") -> c0c29_v0v29 #there are ~7,527 shared genes (cut-off is p-adj of 0.1)
 
 # what about genes unique to c0c29 and v0v29?
 
-str(control0_control29.p) #47866 isogroups
-str(variable0_variable29.p) #47866 isogroups
+str(control0_control29.p) #40,443 genes
+str(variable0_variable29.p) #36,731 genes
 
+str(control0_control29.p %>% filter(abs(lpv_c0c29) >= 1)) #16,022 genes significant at level of padj 0.1 
+str(variable0_variable29.p %>% filter(abs(lpv_v0v29) >= 1)) #10,540 genes significant at level of padj 0.1 
 
+unique_sig_C0C29 <- control0_control29.p %>% filter(abs(lpv_c0c29) >= 1) %>% anti_join(., variable0_variable29.p) #273 genes
 
+unique_sig_V0V29 <- variable0_variable29.p %>% filter(abs(lpv_v0v29) >= 1) %>% anti_join(., control0_control29.p) #0 genes
+
+unique_C0C29 <- anti_join(control0_control29.p, variable0_variable29.p) #3712 genes
 
 
 # next look at variable0vscontrol0 and variable29vscontrol29
@@ -92,7 +81,7 @@ variable0_control0.p %>%
                        quote="", fill=FALSE) %>%
               mutate(gene = V1,
                      KOG = V2) %>%
-              dplyr::select(-V1, -V2), by = "gene") -> v0c0_v29c29 #~1500 shared genes
+              dplyr::select(-V1, -V2), by = "gene") -> v0c0_v29c29 #47 shared genes
 
 
 #### KOG MATCHING ####
@@ -101,50 +90,50 @@ variable0_control0.p %>%
 v0c0_v29c29 %>%
   mutate(KOG = replace(KOG, KOG == "", NA)) %>%
   filter(lpv_v0c0 >= 1) %>%
-  count(KOG) %>%
-  rename("KOG" = KOG, "v0c0_up" = n) -> KOG_v0c0_up
+  dplyr::count(KOG) %>%
+  dplyr::rename("KOG" = KOG, "v0c0_up" = n) -> KOG_v0c0_up
 
 v0c0_v29c29 %>%
   mutate(KOG = replace(KOG, KOG == "", NA)) %>%
   filter(lpv_v0c0 <= -1) %>%
-  count(KOG) %>%
-  rename("KOG" = KOG, "v0c0_down" = n) -> KOG_v0c0_down
+  dplyr::count(KOG) %>%
+  dplyr::rename("KOG" = KOG, "v0c0_down" = n) -> KOG_v0c0_down
 
 v0c0_v29c29 %>%
   mutate(KOG = replace(KOG, KOG == "", NA)) %>%
   filter(lpv_v29c29 >= 1) %>%
-  count(KOG) %>%
-  rename("KOG" = KOG, "v29c29_up" = n) -> KOG_v29c29_up
+  dplyr::count(KOG) %>%
+  dplyr::rename("KOG" = KOG, "v29c29_up" = n) -> KOG_v29c29_up
 
 v0c0_v29c29 %>%
   mutate(KOG = replace(KOG, KOG == "", NA)) %>%
   filter(lpv_v29c29 <= -1) %>%
-  count(KOG) %>%
-  rename("KOG" = KOG, "v29c29_down" = n) -> KOG_v29c29_down
+  dplyr::count(KOG) %>%
+  dplyr::rename("KOG" = KOG, "v29c29_down" = n) -> KOG_v29c29_down
 
 c0c29_v0v29 %>% 
   mutate(KOG = replace(KOG, KOG == "", NA)) %>%
   filter(lpv_c0c29 >= 1) %>%
-  count(KOG) %>%
-  rename("KOG" = KOG, "c0c29_up" = n) -> KOG_c0c29_up
+  dplyr::count(KOG) %>%
+  dplyr::rename("KOG" = KOG, "c0c29_up" = n) -> KOG_c0c29_up
 
 c0c29_v0v29 %>% 
   mutate(KOG = replace(KOG, KOG == "", NA)) %>%
   filter(lpv_c0c29 <= -1) %>%
-  count(KOG) %>%
-  rename("KOG" = KOG, "c0c29_down" = n) -> KOG_c0c29_down
+  dplyr::count(KOG) %>%
+  dplyr::rename("KOG" = KOG, "c0c29_down" = n) -> KOG_c0c29_down
 
 c0c29_v0v29 %>% 
   mutate(KOG = replace(KOG, KOG == "", NA)) %>%
   filter(lpv_v0v29 >= 1) %>%
-  count(KOG) %>%
-  rename("KOG" = KOG, "v0v29_up" = n) -> KOG_v0v29_up
+  dplyr::count(KOG) %>%
+  dplyr::rename("KOG" = KOG, "v0v29_up" = n) -> KOG_v0v29_up
 
 c0c29_v0v29 %>% 
   mutate(KOG = replace(KOG, KOG == "", NA)) %>%
   filter(lpv_v0v29 <= -1) %>%
-  count(KOG) %>%
-  rename("KOG" = KOG, "v0v29_down" = n) -> KOG_v0v29_down
+  dplyr::count(KOG) %>%
+  dplyr::rename("KOG" = KOG, "v0v29_down" = n) -> KOG_v0v29_down
 
 # 1) v0c0_v29c29 KOG class sums in a single dataframe
 
@@ -163,11 +152,11 @@ KOG_c0c29_down %>%
 # melting dataframe for plotting
 KOG_v0c0_v29c29_match %>% 
   melt(id = "KOG") %>% 
-  rename(comparison = variable, sum = value) -> KOG_v0c0_v29c29_melt
+  dplyr::rename(comparison = variable, sum = value) -> KOG_v0c0_v29c29_melt
 
 KOG_c0c29_v0v29_match %>% 
   melt(id = "KOG") %>% 
-  rename(comparison = variable, sum = value) -> KOG_c0c29_v0v29_melt
+  dplyr::rename(comparison = variable, sum = value) -> KOG_c0c29_v0v29_melt
 
 # creating a custom color palette
 colorCount = length(unique(KOG_v0c0_v29c29_match$KOG))
@@ -200,8 +189,7 @@ KOG_c0c29_v0v29_sum <- ggplot(KOG_c0c29_v0v29_melt, aes(fill = KOG, y = sum, x =
   theme_classic() +
   theme(axis.text.x = element_text(angle = 45, vjust = 0.5))
 KOG_c0c29_v0v29_sum
-#ggsave("common genes KOG c0c29_v0v29 abundance.pdf", plot= KOG_c0c29_v0v29_sum, width=8, height=6, units="in", dpi=300)
-
+#ggsave("common genes KOG c0c29_v0v29 abundance.pdf", plot= KOG_c0c29_v0v29_sum, width=12, height=6, units="in", dpi=300)
 
 
 #### VENN DIAGRAMS ####
