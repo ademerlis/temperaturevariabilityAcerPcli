0c3795ad416ed5fdc22c069929a2fb65d32f8489
Allyson DeMerlis <allyson.demerlis@gmail.com>
2023-11-12 18:39:20 -0500

    Update trim_all.sh

--- gene_expression/bioinformatics_pipeline/trim_all.sh
+++ gene_expression/bioinformatics_pipeline/trim_all.sh
@@ -14,14 +14,14 @@ echo '#BSUB -e '"${and}"/Ch2_temperaturevariability2023/2_trimmed_reads/take_3/"
 echo '#BSUB -n 8' >> "${and}"/Ch2_temperaturevariability2023/2_trimmed_reads/take_3/"${sample}"_trim_all.job
 echo '#BSUB -N' >> "${and}"/Ch2_temperaturevariability2023/2_trimmed_reads/take_3/"${sample}"_trim_all.job
 echo 'and="/scratch/projects/and_transcriptomics"' >> "${and}"/Ch2_temperaturevariability2023/2_trimmed_reads/take_3/"${sample}"_trim_all.job
-echo 'cd '"/scratch/projects/and_transcriptomics/Ch2_temperaturevariability2023/1_fastq_rawreads"'' >> "${and}"/Ch2_temperaturevariability2023/2_trimmed_reads/take_3/"${sample}"_trim_all.job
-echo 'SAMP="${sample}"' >> "${and}"/Ch2_temperaturevariability2023/2_trimmed_reads/take_3/"${sample}"_trim_all.job
-echo 'sample_name=$(basename "${SAMP}" .fastq.gz)' >> "${and}"/Ch2_temperaturevariability2023/2_trimmed_reads/take_3/"${sample}"_trim_all.job
-echo 'if [[ "${SAMP}" == *.gz ]]; then' >> "${and}"/Ch2_temperaturevariability2023/2_trimmed_reads/take_3/"${sample}"_trim_all.job
-echo '  gunzip -c "${SAMP}" > "${sample_name}".temp.fastq' >> "${and}"/Ch2_temperaturevariability2023/2_trimmed_reads/take_3/"${sample}"_trim_all.job
-echo '  SAMP="${sample_name}".temp.fastq' >> "${and}"/Ch2_temperaturevariability2023/2_trimmed_reads/take_3/"${sample}"_trim_all.job
+echo 'cd /scratch/projects/and_transcriptomics/Ch2_temperaturevariability2023/1_fastq_rawreads' >> "${and}"/Ch2_temperaturevariability2023/2_trimmed_reads/take_3/"${sample}"_trim_all.job
+echo 'SAMP='"${sample}"'' >> "${and}"/Ch2_temperaturevariability2023/2_trimmed_reads/take_3/"${sample}"_trim_all.job
+echo 'sample_name='$(basename "${SAMP}" .fastq.gz)'' >> "${and}"/Ch2_temperaturevariability2023/2_trimmed_reads/take_3/"${sample}"_trim_all.job
+echo 'if [[ '"${SAMP}" == *.gz ]]; then'' >> "${and}"/Ch2_temperaturevariability2023/2_trimmed_reads/take_3/"${sample}"_trim_all.job
+echo '  gunzip -c '"${SAMP}" '> '"${sample_name}".temp.fastq'''' >> "${and}"/Ch2_temperaturevariability2023/2_trimmed_reads/take_3/"${sample}"_trim_all.job
+echo '  SAMP='"${sample_name}".temp.fastq'' >> "${and}"/Ch2_temperaturevariability2023/2_trimmed_reads/take_3/"${sample}"_trim_all.job
 echo 'fi' >> "${and}"/Ch2_temperaturevariability2023/2_trimmed_reads/take_3/"${sample}"_trim_all.job
-echo 'echo "${SAMP}"' >> "${and}"/Ch2_temperaturevariability2023/2_trimmed_reads/take_3/"${sample}"_trim_all.job
+echo 'echo '"${SAMP}"'' >> "${and}"/Ch2_temperaturevariability2023/2_trimmed_reads/take_3/"${sample}"_trim_all.job
 echo 'lead="${2:-[ATGC]?[ATGC][AC][AT][AT][AC][AT][ACT]GGG+|[ATGC]?[ATGC][AC][AT]GGG+|[ATGC]?[ATGC]TGC[AC][AT]GGG+|[ATGC]?[ATGC]GC[AT]TC[ACT][AC][AT]GGG+}"' >> "${and}"/Ch2_temperaturevariability2023/2_trimmed_reads/take_3/"${sample}"_trim_all.job
 echo '' >> "${and}"/Ch2_temperaturevariability2023/2_trimmed_reads/take_3/"${sample}"_trim_all.job
 echo 'trim=0' >> "${and}"/Ch2_temperaturevariability2023/2_trimmed_reads/take_3/"${sample}"_trim_all.job
@@ -73,7 +73,7 @@ echo '        ll=3' >> "${and}"/Ch2_temperaturevariability2023/2_trimmed_reads/t
 echo '    else' >> "${and}"/Ch2_temperaturevariability2023/2_trimmed_reads/take_3/"${sample}"_trim_all.job
 echo '        ll=2' >> "${and}"/Ch2_temperaturevariability2023/2_trimmed_reads/take_3/"${sample}"_trim_all.job
 echo '    fi' >> "${and}"/Ch2_temperaturevariability2023/2_trimmed_reads/take_3/"${sample}"_trim_all.job
-echo 'done < "${SAMP}"' >> "${and}"/Ch2_temperaturevariability2023/2_trimmed_reads/take_3/"${sample}"_trim_all.job
+echo 'done < '"${SAMP}"'' >> "${and}"/Ch2_temperaturevariability2023/2_trimmed_reads/take_3/"${sample}"_trim_all.job
 echo '' >> "${and}"/Ch2_temperaturevariability2023/2_trimmed_reads/take_3/"${sample}"_trim_all.job
 echo 'if [[ "$seq" =~ ^($lead)(.+) ]]; then' >> "${and}"/Ch2_temperaturevariability2023/2_trimmed_reads/take_3/"${sample}"_trim_all.job
 echo '    start="${BASH_REMATCH[2]:0:20}"' >> "${and}"/Ch2_temperaturevariability2023/2_trimmed_reads/take_3/"${sample}"_trim_all.job
@@ -95,8 +95,8 @@ echo 'elif [ "$tot" -gt 1 ]; then' >> "${and}"/Ch2_temperaturevariability2023/2_
 echo '    nohead=$((nohead + 1))' >> "${and}"/Ch2_temperaturevariability2023/2_trimmed_reads/take_3/"${sample}"_trim_all.job
 echo 'fi' >> "${and}"/Ch2_temperaturevariability2023/2_trimmed_reads/take_3/"${sample}"_trim_all.job
 echo '' >> "${and}"/Ch2_temperaturevariability2023/2_trimmed_reads/take_3/"${sample}"_trim_all.job
-echo 'echo ""${SAMP}"	total:$tot	goods:$goods	dups:$dups	noheader:$nohead	N.in.header:$ntag" > /scratch/projects/and_transcriptomics/Ch2_temperaturevariability2023/2_trimmed_reads/"${SAMP}"_trimoutput.txt' >> "${and}"/Ch2_temperaturevariability2023/2_trimmed_reads/take_3/"${sample}"_trim_all.job
-echo '/scratch/projects/and_transcriptomics/programs/TrimGalore-0.6.10/trim_galore /scratch/projects/and_transcriptomics/Ch2_temperaturevariability2023/1_fastq_rawreads/"${SAMP}".trim.fastq \' >> "${and}"/Ch2_temperaturevariability2023/2_trimmed_reads/take_3/"${sample}"_trim_all.job
+echo 'echo '""${SAMP}"	total:$tot	goods:$goods	dups:$dups	noheader:$nohead	N.in.header:$ntag" '> /scratch/projects/and_transcriptomics/Ch2_temperaturevariability2023/2_trimmed_reads/'"${SAMP}"_trimoutput.txt'''' >> "${and}"/Ch2_temperaturevariability2023/2_trimmed_reads/take_3/"${sample}"_trim_all.job
+echo '/scratch/projects/and_transcriptomics/programs/TrimGalore-0.6.10/trim_galore /scratch/projects/and_transcriptomics/Ch2_temperaturevariability2023/1_fastq_rawreads/'"${SAMP}".trim.fastq \''' >> "${and}"/Ch2_temperaturevariability2023/2_trimmed_reads/take_3/"${sample}"_trim_all.job
 echo '--adapter "AGATCGG" \' >> "${and}"/Ch2_temperaturevariability2023/2_trimmed_reads/take_3/"${sample}"_trim_all.job
 echo '--adapter "AAAAAAAA" \' >> "${and}"/Ch2_temperaturevariability2023/2_trimmed_reads/take_3/"${sample}"_trim_all.job
 echo '--quality 15 \' >> "${and}"/Ch2_temperaturevariability2023/2_trimmed_reads/take_3/"${sample}"_trim_all.job
