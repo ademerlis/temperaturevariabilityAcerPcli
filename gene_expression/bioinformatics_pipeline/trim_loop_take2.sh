c5f3b3027652ba7159d2e58cdb4f4022a7f5ead9
Allyson DeMerlis <allyson.demerlis@gmail.com>
2023-11-13 16:40:13 -0500

    new trim loop script

--- gene_expression/bioinformatics_pipeline/trim_loop_take2.sh
+++ gene_expression/bioinformatics_pipeline/trim_loop_take2.sh
@@ -20,16 +20,21 @@ echo "
 #BSUB -e ${projdir}/logs/${samp}_trim.err
 #BSUB -o ${projdir}/logs/${samp}_trim.out
 #BSUB -W 12:00
-#BSUB -n 16
-#BSUB -q bigmem
+#BSUB -n 8
+#BSUB -q general
+
+cd \"/scratch/projects/and_transcriptomics/Ch2_temperaturevariability2023/1_fastq_rawreads\"
 
 echo \"Gunzipping file  ${samp} then removing adaptors and low quality reads...\"
 
-gunzip -c ${samp} | ${and}/Ch2_temperaturevariability2023/0_scripts/tagseq_clipper.pl | cutadapt - -a AAAAAAAA -a AGATCGG -q 15 -m 25 -o ${projdir}/${samp/.fastq/}.trim
+gunzip -c ${samp} > ${samp}_temp.fastq
+${and}/Ch2_temperaturevariability2023/0_scripts/tagseq_clipper.pl ${samp}_temp.fastq | cutadapt - -a AAAAAAAA -a AGATCGG -q 15 -m 25 -o ${projdir}/${samp/.fastq.gz/}.trim
 
 " > ${projdir}/${samp}_trim.job
+
 #submit script
-bsub < ${projdir}/${samp}/${samp}_trim.job
+
+bsub < ${projdir}/${samp}_trim.job
 
 done
 
